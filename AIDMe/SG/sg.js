20ad099a9866b3b8fa341fe12416fe4e34d9afd0
Carlos Aguilar <capagp@gmail.com>
2018-06-13 18:32:41 -0500

    sdf

--- AIDMe/SG/sg.js
+++ AIDMe/SG/sg.js
@@ -1,13 +1,28 @@
 "use strict";
 
-window.onload = function() {
-    function() {
-	var link = document.querySelector('link[rel="import"]');;
-	var content = link.import;
+var app = {};
 
-	// HEAD
-	content.querySelectorAll('div').forEach( e => document.body.appendChild(e.cloneNode(true)));
-    }();
+window.onload = function() {
+	let asnum = function(s) {
+	    let n = Number(s);
+	    return (Number.isNaN(n) ? 0 : n);
+	}
+
+	let PZ = document.getElementById('cart').firstChild.firstChild;
+
+	app.add2cart = function(e) {
+	    const v = asnum(e.value);
+	    const n = e.name;
+	    if (v > 0) {
+		sessionStorage.setItem(n, v);
+		let v1 = v + asnum(e.parentElement.textContent);
+		sessionStorage.setItem(n, v1);
+		console.log(sessionStorage.getItem(n));
+		PZ.textContent = v1;
+	    } else
+		PZ.textContent = '';
+	};
+
 };
 
 
