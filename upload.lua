ac895d4599f3dce4ace8d7b3eb268aad209e60f7
SmartGoggles <sg@medaid02.medaid.app>
2018-10-08 14:01:06 -0500

    sdf90kjdsf

--- upload.lua
+++ upload.lua
@@ -19,15 +19,35 @@ end
 local ret = sites.html()
 
 local style = [==[
-#container {
+input {
+    padding: 0.5rem;
+    border-radius: 4px;
+    border: 1px solid #ddd;
+}
+
+input:invalid + span:after {
+    content: 'x';
+    color: #f00;
+    padding-left: 5px;
+}
+
+#mybox {
     margin-top: 95px;
     width: 95%;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: space-around;
 }
 ]==]
 
 local body = [==[
-    <div id="container">
-	<div id="filelist"></div>
+    <div id="mybox">
+	<label for="nombre">Nombre</label>
+	<select id="nombre" name="nombre" required >
+	</select>
+	<label>Registro <input id="registro" name="registro" type="text" maxlength="6" minlength="3" value="01256" required /></label><span></span>
+	<br />
+	<div id="container"><div id="filelist"></div></div>
 	<br />
 	<a id="pickfiles" href="javascript:;">[Seleccionar]</a>
 	<a id="uploadfiles" href="javascript:;">[Enviar]</a>
@@ -38,9 +58,19 @@ local body = [==[
 
 local script = [==[
     let flist = document.getElementById("filelist");
+    let myname = document.getElementById("nombre");
+    let myreg = document.getElementById("registro");
+
+    var NOMBRES = ['', ];
+    XHR.getJSON("nombres.lua").then( a => a.forEach( p => { NOMBRES[p.id] = p.nombre; } ) );
+
     let uploader = new plupload.Uploader({ 
 	runtimes: "html5, html4",
 	browse_button: "pickfiles",
+	container: "container",
+	url: "uploadVideo.lua",
+	multipart: true,
+	multipart_params: {},
 
 	filters : {
 	    max_file_size : "50mb",
@@ -53,7 +83,8 @@ local script = [==[
 
 	init: {
 	    PostInit: function() {
-		document.getElementById('filelist').onclick = function() {
+		flist.innerHTML = "";
+		document.getElementById("uploadfiles").onclick = function() {
 		    uploader.start();
 		    return false;
 		};
@@ -62,6 +93,12 @@ local script = [==[
 		plupload.each(files, function(file) {
 		    flist.innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
 		});
+	    },
+	    BeforeUpload: function(up, files) {
+		up.settings.multipart_params = {
+		    registro : myreg.value,
+		    nombre : myname.value
+		};
 	    },
 	    UploadProgress: function(up, file) {
 		document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + '%</span>';
